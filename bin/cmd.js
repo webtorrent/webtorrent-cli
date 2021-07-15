1a7583044f14c34d1d4c76ec526ae446ecd461ac
Osman Altun <osmanaltun1102@gmail.com>
2021-07-15 17:36:22 +0200

    fix: multiple torrent downloads (#177)
    * fix: multiple torrent downloads
    
    * Update bin/cmd.js
    
    Co-authored-by: Diego Rodríguez Baquero <github@diegorbaquero.com>
    
    Co-authored-by: Diego Rodríguez Baquero <github@diegorbaquero.com>

--- bin/cmd.js
+++ bin/cmd.js
@@ -794,7 +794,7 @@ function getRuntime () {
 function processInputs (inputs, fn) {
   // These arguments do not make sense when downloading multiple torrents, or
   // seeding multiple files/folders.
-  if (inputs) {
+  if (Array.isArray(inputs) && inputs.length !== 0) {
     if (inputs.length > 1) {
       const invalidArguments = [
         'airplay', 'chromecast', 'dlna', 'mplayer', 'mpv', 'omx', 'vlc', 'iina', 'xbmc',
@@ -810,9 +810,8 @@ function processInputs (inputs, fn) {
       })
       torrentCount = inputs.length
       enableQuiet()
-    } else if (inputs.length === 1) {
-      fn(inputs[0])
     }
+    inputs.forEach(input => fn(input))
   } else {
     yargs.showHelp('log')
   }
