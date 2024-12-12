8e88bddca17e993120f8c9995f80a55f06786bf4
Kian-Meng Ang <kianmeng.ang@gmail.com>
2024-12-12 23:28:55 +0800

    docs: fix typos (#323)
    Found via `codespell -L mey,wirth`

--- version.cjs
+++ version.cjs
@@ -1,2 +1,2 @@
-// Exports package.json version to work around "with" and "assert" imports for backwards compatability.
+// Exports package.json version to work around "with" and "assert" imports for backwards compatibility.
 module.exports = require('./package.json').version
